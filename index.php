da4bb04f3a7daec4fedfe1fa7180cc24538124e5
barrychum <barrychum@users.noreply.github.com>
2021-08-20 18:37:03 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
+echo 'HTTP remote address :<br>';
 echo $_SERVER['REMOTE_ADDR'].'<br>';
+echo '<br><br>';
+echo 'HTTP forwarded address :<br>';
 echo $_SERVER['HTTP_X_FORWARDED_FOR'].'<br>';
 ?>
